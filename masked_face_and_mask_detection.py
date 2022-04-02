7cf259e7e9c141bcfbe282357b926ddf020f2baf
Samir Asif <74524705+Samir529@users.noreply.github.com>
2022-04-02 19:39:09 +0600

    commit

--- masked_face_and_mask_detection.py
+++ masked_face_and_mask_detection.py
@@ -132,7 +132,29 @@ faces = ['Abdur Samad', 'Ahsan Ahmed', 'Asef', 'Ashik', 'Azizul Hakim', 'DDS', '
 
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    # {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+{
+  "iceServers": [
+    {
+      "urls": ["stun:openrelay.metered.ca:80"],
+    },
+    {
+      "urls": ["turn:openrelay.metered.ca:80"],
+      "username": "openrelayproject",
+      "credential": "openrelayproject",
+    },
+    {
+      "urls": ["turn:openrelay.metered.ca:443"],
+      "username": "openrelayproject",
+      "credential": "openrelayproject",
+    },
+    {
+      "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
+      "username": "openrelayproject",
+      "credential": "openrelayproject",
+    },
+  ],
+}
 )
 # RTC_CONFIGURATION = RTCConfiguration(
 #     {"iceServers": [{"urls": ["stun:stun.xten.com:3478"]}]}
